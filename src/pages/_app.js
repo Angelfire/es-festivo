1e3d04d30da2f1966911a919e7e4adbc39be9ab6
Andres Bedoya <mortiis.angel@gmail.com>
2021-11-18 18:42:12 -0500

    remove no zooming on meta

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover" />
       </Head>
       <Component {...pageProps} />
     </>
