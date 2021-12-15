1deaa3b0d4b84d9c523d13e4a16d1d1f6ff0e5ba
Andres Bedoya <mortiis.angel@gmail.com>
2021-12-14 20:59:50 -0500

    fix local time

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -13,20 +13,17 @@ const formatDate = (date) => {
 const getColombianHolidays = (year = new Date().getFullYear()) => {
   const currentDate = new Date()
   const sumYears = [...colombianHolidays(year), ...colombianHolidays(year + 1)]
-  const clearCurrentDate = currentDate.setHours(0, 0, 0, 0)
   const fullHolidays = sumYears.map((holiday, index) => {
     const holidayDateT = new Date(`${holiday.holiday.replace(/\//g, '-')}T00:00:00.000-05:00`)
-    const daysCountNextHoliday = (new Date(holiday.holiday).getTime() - new Date(clearCurrentDate)) / (1000 * 60 * 60 * 24)
+    const daysCountNextHoliday = Math.ceil((new Date(holiday.holiday).getTime() - new Date(currentDate).getTime()) / (1000 * 60 * 60 * 24))
 
     return {
       index,
-      diff: new Date(clearCurrentDate).getTime() - holidayDateT.getTime(),
+      diff: new Date(currentDate).getTime() - holidayDateT.getTime(),
       holidayDate: holiday.holiday,
       holidayCountNext: daysCountNextHoliday,
       holidayName: holiday.holidayName,
-      holidayLocaleDate: formatDate(holidayDateT),
-      holi: currentDate,
-      holi2: new Date(clearCurrentDate)
+      holidayLocaleDate: formatDate(holidayDateT)
     }
   })
 
